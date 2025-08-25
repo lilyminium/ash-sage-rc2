eee2d80d9587d17fcad2ae57faaa25b2a5bec871
Lily Wang <lily@mdanalysis.org>
2025-08-25 12:20:50 +1000

    update files

--- 01_download-data/qm/README.md
+++ 01_download-data/qm/README.md
@@ -2,7 +2,7 @@
 
 The scripts here download initial data from the MolSSI QCArchive:
 
-* optimization and torsiondrive data
-* Hessian data
+* optimization and torsiondrive data (for training and testing)
+* Hessian data (for MSM values)
 
 Please see `run-download.sh` for input/output options, and *.log files for logging output.
--- 01_download-data/qm/run-download.sh
+++ 01_download-data/qm/run-download.sh
@@ -3,7 +3,7 @@
 
 python download-qcdata-tables.py -o data/tables > download.log 2>&1
 
-# python download-hessian-data.py                         \
-#     -i      data/tables/optimization                    \
-#     -o      hessian-data                                    \
-#     -x      ../../03_fit-valence/02_curate-data/bad-data/bad-qcarchive_ids.dat > download-hessian.log 2>&1
+python download-hessian-data.py                         \
+    -i      data/tables/optimization                    \
+    -o      hessian-data                                    \
+    -x      ../../03_fit-valence/02_curate-data/bad-data/bad-qcarchive_ids.dat > download-hessian.log 2>&1
